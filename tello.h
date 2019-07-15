51bc2d307cbbd714387ad975ad648e8224ea65ce
K-D-G <grayshonprogrammer@gmail.com>
2019-07-15 20:25:15 +0100

    Started adding C++ support

---
+++ tello.h
@@ -0,0 +1,16 @@
+#ifndef TELLO_WRAPPER_H
+#define TELLO_WRAPPER_H
+#include <iostream>
+#include <boost/asio.hpp>
+
+namespace TelloWrapper{
+  class Tello{
+    Tello();
+    ~Tello();
+
+
+  };
+}
+
+
+#endif
